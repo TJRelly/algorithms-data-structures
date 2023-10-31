fa399ecc5e3e35ff4c5327ce7e23f44f2797883a
Terrell Jackson <94482664+TJRelly@users.noreply.github.com>
2023-10-30 21:40:45 -0400

    double loop

--- areThereDuplicates.js
+++ areThereDuplicates.js
@@ -2,18 +2,17 @@ function areThereDuplicates(...args) {
     //frequency counter
     //convert array to object
     //if any value = 2 return true else false
-    let obj = args.reduce((obj, curr) => {
-        obj[curr] ? obj[curr]++ : obj[curr] = 1
-        return obj
-    }, {})
-
-    for(let key in obj) {
-        if(obj[key] === 2) return true
+    let i = 0
+    for (let j = 1; j < args.length; j++){
+        if(args[i] === args[j]) {
+            i++
+            args[i] = args[j]
+        }
+        console.log(i, j)
     }
-
-    return false
+    return i > 0 ? true : false
   }
 
-  areThereDuplicates(1, 2, 3) // false
-  areThereDuplicates(1, 2, 2) // true 
-  areThereDuplicates('a', 'b', 'c', 'a') // true 
+  console.log(areThereDuplicates(1, 2, 3)) // false
+  console.log(areThereDuplicates(1, 2, 2)) // true 
+  console.log(areThereDuplicates('a', 'b', 'c', 'a')) // true 
