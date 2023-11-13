aec7ffe130240642f3c9fa99d8dbabd9dfa0322d
Terrell Jackson <94482664+TJRelly@users.noreply.github.com>
2023-11-13 15:50:33 -0500

    set

---
+++ countUniqueValues.js
@@ -0,0 +1,11 @@
+//Multiple Pointers
+
+function countUniqueValues(arr) {
+    let set = new Set(arr)
+    return set.size
+}
+
+console.log(countUniqueValues([])) //0
+console.log(countUniqueValues([1, 1, 1, 1, 2])) //2
+console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13])) //7
+console.log(countUniqueValues([-2, -1, -1, 0, 1])) //4
