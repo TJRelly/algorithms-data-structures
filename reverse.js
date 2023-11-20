7da838802b7ed89f95a07a3502f4db2ce054f1da
Terrell Jackson <94482664+TJRelly@users.noreply.github.com>
2023-11-20 10:48:15 -0500

    sumToN

--- reverse.js
+++ reverse.js
@@ -11,6 +11,24 @@ const reverseStr2 = str => {
     return reversed
 }
 
+console.log("-- Reversed String --")
 console.log(reverseStr("Hello World!"))
-console.log("")
 console.log(reverseStr2("Hello World!"))
+console.log("")
+
+//function calculates the sum of all nums up to some number n
+function sumToN(n) {
+    //declare sum variable
+    let sum = 0
+    // loop through numbers from 1 to n
+    for(let i = 1; i <= n; i++) {
+        sum += i
+    }
+    //adding each to sum variable
+    return sum
+}
+
+console.log("-- Sum to N --")
+console.log(sumToN(5))//should return 15
+console.log(sumToN(3))//should return 6
+console.log("")
