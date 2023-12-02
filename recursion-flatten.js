e9892af50accf9b9c4ef5e72d07fd8b22aee7a30
Terrell Jackson <94482664+TJRelly@users.noreply.github.com>
2023-12-02 00:08:27 -0500

    closure

--- recursion-flatten.js
+++ recursion-flatten.js
@@ -1,11 +1,7 @@
 //input: array of arrays
 //output: flattened array
 function flatten(arrayOfArrays){
-    //base case: empty array return empty array
-    if(arrayOfArrays.length === 0) return []
-    if(Array.isArray(arrayOfArrays[0])) return flatten(arrayOfArrays[0]).concat(flatten(arrayOfArrays.slice(1)))
-    //recursive case: slice the array
-    else return [arrayOfArrays[0]].concat(flatten(arrayOfArrays.slice(1)))
+   
   }
   
   console.log(flatten([1, 2, 3, [4, 5] ])) // [1, 2, 3, 4, 5]
