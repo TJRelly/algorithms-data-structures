3f9f0f271cab31ba3be3a2791540053c433de3c1
Terrell Jackson <94482664+TJRelly@users.noreply.github.com>
2023-12-03 14:20:40 -0500

    recursion

--- recursion-flatten.js
+++ recursion-flatten.js
@@ -1,15 +1,11 @@
 //input: array of arrays
 //output: flattened array
-function flatten(arrayOfArrays, flat=[]){
-    //base case: empty input array will return an empty array
-    if(arrayOfArrays.length === 0) return flat
-    //if the first element of the array is an array, run recursion on that element
-    //otherwise push first element to result
-    if(Array.isArray(arrayOfArrays[0])) flatten(arrayOfArrays[0], flat)
-    //recursive case: push the first element of array into the empty array
-    else flat.push(arrayOfArrays[0])
-
-    return flatten(arrayOfArrays.slice(1), flat)
+function flatten(arrayOfArrays){
+    //base case: empty array return empty array
+    if(arrayOfArrays.length === 0) return []
+    if(Array.isArray(arrayOfArrays[0])) return flatten(arrayOfArrays[0]).concat(flatten(arrayOfArrays.slice(1)))
+    //recursive case: slice the array
+    else return [arrayOfArrays[0]].concat(flatten(arrayOfArrays.slice(1)))
   }
   
   console.log(flatten([1, 2, 3, [4, 5] ])) // [1, 2, 3, 4, 5]
